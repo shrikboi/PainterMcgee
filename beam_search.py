fd544be70f8df73bb5533b4b470ca373d8c7f1b9
Ilia <ilia.efremov@mail.huji.ac.il>
2024-08-21 17:09:41 +0300

    beam search looks good

--- beam_search.py
+++ beam_search.py
@@ -1,50 +1,59 @@
-import numpy as np
+import os
 import cv2
+
+import numpy as np
 from tqdm import tqdm
 from typing import List, Tuple
 import random
-from utils import Rectangle, draw_rectangle, PICTURE_SIZE, calculate_color, display_images_side_by_side
+import time
+import abc
 
-RANDOM_TILES_PER_ROUND = 20
-NUM_OF_BRUSHSTROKES = 500
-# NUM_OF_TILES = 1000
+import colour
+
+import skimage as ski
+from utils import (draw_rectangle, PICTURE_SIZE, generate_random_rectangle, create_gif, display_images_side_by_side,
+                   display_image, extract_features)
+
+RANDOM_TILES_PER_ROUND = 10
+NUM_OF_BRUSHSTROKES = 120
 BEAM_WIDTH = 5
 ITER_NUM = 1000
 RECTANGLE_LIST = './layouts/rectangle_list.txt'
 IMAGE_FILENAME = './layouts/FELV-cat.jpg'
+MAX_SIZE = 20
+OUTPUT_FOLDER = './output_paintings'
+OUTPUT_GIF = './output_gif.gif'
+MULTIPLY_WEIGHTS = 50
 
 
-def load_rectangle_list(filename):
-    rectangles = []
-    with open(filename, 'r') as f:
-        for line in f:
-            width, height, angle, edge_thickness = map(int, line.strip().split())
-            rectangles.append((width, height, angle, edge_thickness))
-    return rectangles
+class Node:
+    def __init__(self, canvas, stroke_set, loss, parent=None):
+        self.canvas = canvas
+        self.stroke_set = stroke_set
+        self.loss = loss
+        self.parent = parent
+
+    def get_path(self) -> List:
+        path = []
+        node = self
+        while node is not None:
+            path.append(node)
+            node = node.parent
+        return path[::-1]  # return reversed path
 
 
-def get_empty_canvas(target_img) -> np.ndarray:
+def get_empty_canvas() -> np.ndarray:
     """
     returns an empty(white) canvas the size of the target image
     """
     return np.ones((PICTURE_SIZE[1], PICTURE_SIZE[0], 3), dtype=np.uint8) * 255
 
 
-def create_random_tile(rectange_list: List, target_image) -> Rectangle:
-    """
-    generates random tile with x,y location
-    """
-
-    width, height, angle, edge_thickness = random.choice(rectange_list)
-    center_x = random.randint(0, PICTURE_SIZE[0] - 1)
-    center_y = random.randint(0, PICTURE_SIZE[1] - 1)
-    color = calculate_color((width, height), (center_x, center_y), angle, target_image)
-    opacity = random.choice([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
-    return Rectangle((width, height), angle, edge_thickness, (center_x, center_y), color, opacity)
-
-
-def get_tile_set(rectangle_list: List, target_image, set_size: int):
-    return [create_random_tile(rectangle_list, target_image) for _ in range(set_size)]
+def get_tile_set(tgt_image, set_size: int,  max_block_size, radomize_color=True):
+    tiles = []
+    for _ in range(set_size):
+        tiles.append(generate_random_rectangle(tgt_image, max_block_size, 0, color_random=radomize_color))
+    return tiles
 
 
 def apply_brushstrokes(canvas, brushstrokes):
@@ -53,82 +62,117 @@ def apply_brushstrokes(canvas, brushstrokes):
     return canvas
 
 
-def mse_loss(image, target_image) -> float:
-    return np.sum((image.astype(np.float32) - target_image.astype(np.float32)) ** 2)
-
-def evaluate_canvas():
-    pass
+def mse_loss(image, tgt_image) -> float:
+    return np.sum((image.astype(np.float32) - tgt_image.astype(np.float32)) ** 2)
 
 
-def random_init_canvas(target_image, rectangle_list) -> Tuple:
+def delta_e_loss(image, image2, weights_matrix):
+    image1 = cv2.cvtColor(image.astype(np.float32) / 255, cv2.COLOR_RGB2Lab)
+    image2 = cv2.cvtColor(image2.astype(np.float32) / 255, cv2.COLOR_RGB2Lab)
+
+    delta_e = colour.difference.delta_e.delta_E_CIE2000(image1, image2)
+    delta_e = delta_e * weights_matrix
+    return np.mean(delta_e)
+
+
+def random_init_canvas(tgt_image) -> Tuple:
     """
     creates a canvas that was randomly initialized
     """
-    canvas = get_empty_canvas(target_image)
-    stroke_set = get_tile_set(rectangle_list, target_image, NUM_OF_BRUSHSTROKES)
+    canvas = get_empty_canvas()
+    stroke_set = get_tile_set(tgt_image, NUM_OF_BRUSHSTROKES, MAX_SIZE)
 
     canvas = apply_brushstrokes(canvas, stroke_set)
     return canvas, stroke_set
 
 
-def beam_search(target_image, rectangle_list, n_brushstrokes=NUM_OF_BRUSHSTROKES, beam_width=BEAM_WIDTH,
+def save_paintings_to_folder(directory, node_list: List[Node], side_by_side=False, og_photo=None):
+    """
+    Save the paintings represented by the nodes in the beam search to a folder.
+
+    :param directory: The directory where the images will be saved.
+    :param node_list: List of Node objects, each representing a painting state.
+    :param side_by_side: Boolean indicating whether to display the original photo alongside the generated painting.
+    :param og_photo: The original photo to display alongside the painting if side_by_side is True.
+    """
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for i, node in enumerate(node_list):
+        painting = np.ones((*PICTURE_SIZE[::-1], 3), dtype=np.uint8) * 255
+        painting = apply_brushstrokes(painting, node.stroke_set)
+
+        if side_by_side and og_photo is not None:
+            combined_image = display_images_side_by_side(og_photo, painting)
+            cv2.imwrite(os.path.join(directory, f"image_{i}.png"), combined_image)
+        else:
+            cv2.imwrite(os.path.join(directory, f"image_{i}.png"), display_image(painting))
+
+
+def beam_search(tgt_image, weight_matrix,  n_brushstrokes=NUM_OF_BRUSHSTROKES, beam_width=BEAM_WIDTH,
                 n_candidates=RANDOM_TILES_PER_ROUND, iterations=ITER_NUM):
     """
     performs a bram search and returns a list of blocks that represent the painting
     """
-    # setup random init canvas
-    # run iterations
-    # get random tileset
-    # get tile to replace
-    # replace with the best option
+    #  add color options(delta e from genetic boi),
+    #  and after it works implement the SSIM loss
 
-    init_canvas, init_stroke_set = random_init_canvas(target_image, rectangle_list)
-    init_loss = mse_loss(init_canvas, target_image)
-    beam = [(init_canvas, init_stroke_set, init_loss)]
+    init_canvas, init_stroke_set = random_init_canvas(tgt_image)
+    init_loss = mse_loss(init_canvas, tgt_image)
+    # init_loss = delta_e_loss(init_canvas, tgt_image, weights_matrix)
+    beam = [Node(init_canvas, init_stroke_set, init_loss)]
+
+    # cv2.imshow('Initial Canvas', init_canvas)
+    # cv2.waitKey(0)
+
+    cv2.imwrite('./init_image.jpeg', init_canvas)
 
     for _ in tqdm(range(iterations)):
-        new_beam = []
+        new_beam: List[Node] = []
 
-        for _, stroke_set, _ in beam:
+        for node in beam:
+            stroke_set = node.stroke_set
             index_to_modify = random.randint(0, n_brushstrokes - 1)
-            new_tiles = get_tile_set(rectangle_list, target_image, n_candidates)
+            new_tiles = get_tile_set(tgt_image, n_candidates, MAX_SIZE)
 
             for new_tile in new_tiles:
                 new_stroke_set = stroke_set.copy()
                 new_stroke_set[index_to_modify] = new_tile
 
-                new_canvas = get_empty_canvas(target_image)
+                new_canvas = get_empty_canvas()
                 new_canvas = apply_brushstrokes(new_canvas, new_stroke_set)
 
-                new_loss = mse_loss(new_canvas, target_image)
-                new_beam.append((new_canvas, new_stroke_set, new_loss))
+                # new_loss = mse_loss(new_canvas, tgt_image)
+                new_loss = delta_e_loss(init_canvas, tgt_image, weights_matrix)
+                new_node = Node(new_canvas, new_stroke_set, new_loss, parent=node)
+                new_beam.append(new_node)
 
-        beam = sorted(new_beam, key=lambda x: x[2])[:beam_width]
+        beam = sorted(new_beam, key=lambda x: x.loss)[:beam_width]
 
     return beam[0]
-
-    # cv2.imshow('Initial Canvas', init_canvas)
-    # cv2.waitKey(0)
 
 
 if __name__ == '__main__':
     target_image = cv2.imread(IMAGE_FILENAME)
     target_image = cv2.resize(target_image, PICTURE_SIZE)
 
-    rectangle_list = load_rectangle_list(RECTANGLE_LIST)
+    weights_matrix = extract_features(target_image, MULTIPLY_WEIGHTS)
 
-    final_canvas, final_stroke_set, final_loss = beam_search(
+    final_node = beam_search(
         target_image,
-        rectangle_list,
+        weights_matrix,
         n_brushstrokes=NUM_OF_BRUSHSTROKES,
         beam_width=BEAM_WIDTH,
         n_candidates=RANDOM_TILES_PER_ROUND,
         iterations=ITER_NUM
     )
-    result_image = display_images_side_by_side(target_image, final_canvas)
-    cv2.imshow("Target vs Result", result_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
+    path = final_node.get_path()  # Get the path from the final node
 
+    save_paintings_to_folder(OUTPUT_FOLDER, path, side_by_side=True, og_photo=target_image)
+    create_gif(OUTPUT_FOLDER, OUTPUT_GIF)
 
+    # result_image = display_images_side_by_side(target_image, final_node.canvas)
+    # cv2.imshow("Target vs Result", result_image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
